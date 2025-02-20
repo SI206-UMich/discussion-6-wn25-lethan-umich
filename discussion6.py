d11991774c3f8f16c9de941d46b815ef82be627e
lethan-umich <lethan@umich.edu>
2025-02-20 11:17:36 -0500

    finished

--- discussion6.py
+++ discussion6.py
@@ -1,6 +1,9 @@
 import unittest
 import os
+import csv
+import statistics
 
+from collections import defaultdict
 
 def load_csv(f):
     '''
@@ -18,8 +21,17 @@ def load_csv(f):
     base_path = os.path.abspath(os.path.dirname(__file__))
     full_path = os.path.join(base_path, f)
     # use this 'full_path' variable as the file that you open
+    data = defaultdict(dict)
+    with open(full_path) as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+        for row in reader:
+            data[headers[1]][row[0]] = row[1]
+            data[headers[2]][row[0]] = row[2]
+            data[headers[3]][row[0]] = row[3]
+    return data
 
-def get_annual_max(d):
+def get_annual_max(d: dict[str, dict[str, str]]):
     '''
     Params:
         d, dict created by load_csv above
@@ -31,9 +43,19 @@ def get_annual_max(d):
     Note: Don't strip or otherwise modify strings. Do not change datatypes except where necessary.
         You'll have to change vals to int to compare them. 
     '''
-    pass
-
-def get_month_avg(d):
+    data = []
+    for k, v in d.items():
+        max_val = 0
+        max_month = ""
+        for month, value in v.items():
+            num = int(value)
+            if num > max_val:
+                max_month = month
+                max_val = num
+        data.append((k, max_month, max_val))
+    
+    return data
+def get_month_avg(d: dict[str, dict[str, str]]):
     '''
     Params: 
         d, dict created by load_csv above
@@ -45,7 +67,11 @@ def get_month_avg(d):
     Note: Don't strip or otherwise modify strings. Do not change datatypes except where necessary. 
         You'll have to make the vals int or float here and round the avg to pass tests.
     '''
-    pass
+    data = {}
+    for year, year_data in d.items():
+        data[year] = statistics.mean(map(int, year_data.values()))
+
+    return data
 
 class dis7_test(unittest.TestCase):
     '''
